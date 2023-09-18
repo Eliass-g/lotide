cad0c233551ef2fbfb629ec675b1e0387e143f27
Eliass <eliass.ghauss@email.com>
2023-09-18 17:50:32 -0400

    added tail function

---
+++ tail.js
@@ -0,0 +1,23 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr) {
+  let result = [];
+  for (let i = 1; i < arr.length; i++) {
+    result.push(arr[i]);
+  }
+  return result;
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
