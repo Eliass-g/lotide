6e7419eb3045df80e1b9ee8b83c6ed73c568de5f
Eliass <eliass.ghauss@email.com>
2023-09-20 18:31:42 -0400

    added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (actual !== expected) {
+    return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(obj, val) {
+  for (let key in obj) {
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return;
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
