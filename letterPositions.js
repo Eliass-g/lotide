42f391cc1481983ee6492414414a4065b461ee47
Eliass <eliass.ghauss@email.com>
2023-09-20 17:30:55 -0400

    added letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,40 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected)) {
+    return console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
+  }
+  if (!eqArrays(actual, expected)) {
+    return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  let i = 0;
+  // logic to update results here
+  for (let letter of sentence) {
+    if (letter !== " ") { 
+      if (results[letter] === undefined) {
+        results[letter] = [i];
+      } else {
+        results[letter].push(i);
+      }
+    }
+    i++;
+  }
+  return results;
+};
+
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22]);
+assertArraysEqual(letterPositions("lighthouse in the house").e, [9, 16, 22, 25]);
