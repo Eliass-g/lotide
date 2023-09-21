02389d33a7efe3031119f3f90075b9d37e1adda8
Eliass-g <eliass.ghauss@gmail.com>
2023-09-21 19:36:11 -0400

    added undefined test case to findKey.js

--- findKey.js
+++ findKey.js
@@ -33,5 +33,15 @@ const akaleri = (findKey({
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 3));
 
+const und = (findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4));
+
 assertEqual(noma, "noma");
 assertEqual(akaleri, "Akaleri");
+assertEqual(und, undefined);
