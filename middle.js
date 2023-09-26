662234e5176fe467528651b8c51063954489e014
Eliass-g <eliass.ghauss@gmail.com>
2023-09-26 02:22:33 -0400

    finalized before implementing mocha

--- middle.js
+++ middle.js
@@ -1,24 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    return console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (!eqArrays(actual, expected)) {
-    return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const middle = function(array) {
   lengthOfArray = array.length;
   let result = [];
@@ -34,9 +13,4 @@ const middle = function(array) {
   return result;
 }
 
-assertArraysEqual((middle([1])), []);
-assertArraysEqual((middle([1, 2])), []);
-assertArraysEqual((middle([1, 2, 3])), [2]);
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]);
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]);
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]);
+module.exports = middle;
