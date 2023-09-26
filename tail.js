e91dd6f8d9b2de24d7697d6479ca24c4fcf49196
Eliass <eliass.ghauss@email.com>
2023-09-26 02:22:33 -0400

    finalized before implementing mocha

--- tail.js
+++ tail.js
@@ -1,13 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
-  }
-  if (actual !== expected) {
-    return console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(arr) {
   let result = [];
   for (let i = 1; i < arr.length; i++) {
@@ -16,8 +6,4 @@ const tail = function(arr) {
   return result;
 };
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
